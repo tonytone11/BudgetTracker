0b6e2f96e97833728590fe8c666f04a3a2f2156a
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-23 01:27:03 -0500

    encapsulating methods to calculate budget, income, & expenses

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ class Budget {
         this.income = [];  // this will store the income entries
         this.expenses = [];  // this will store the expenses entries
     }
+    /* Encapsulating Phase */
+    getTotalBudget() { // method to calculate income minus expenses
+        return this.getTotalIncome() - this.getTotalExpenses();
+    }
+    getTotalIncome() { // method to calculate all the income amounts in the this.income array
+        return this.income.reduce((total, item) => total + item.amount, 0); // reduce() method will take all amounts and add them together
+    }
+    getTotalExpenses() { // method to calculate all the expenses amounts in the this.expenses array
+        return this.expenses.reduce((total, item) => total + item.amount, 0);
+    }
 }
-
-// 
