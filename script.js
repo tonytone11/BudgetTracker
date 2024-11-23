94acd3478a766bf3be45bd57225b9e57548c4b4b
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-23 01:19:03 -0500

    created Budget class

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Building functionality for a dynamic Budget Summary */
+// Will create a Budget class to handle income, expenses, and total budget
+class Budget {
+    constructor() {
+        this.income = [];  // this will store the income entries
+        this.expenses = [];  // this will store the expenses entries
+    }
+}
+
+// 
