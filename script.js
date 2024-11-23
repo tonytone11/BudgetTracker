2651f6860014f530626b7cb35e4a490b020ab1b6
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-23 01:48:19 -0500

    added methods that add the entries into my arrays

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /* Building functionality for a dynamic Budget Summary */
-// Will create a Budget class to handle income, expenses, and total budget
-class Budget {
+class Budget { // Will create a Budget class to handle income, expenses, and total budget
     constructor() {
         this.income = [];  // this will store the income entries
         this.expenses = [];  // this will store the expenses entries
@@ -14,5 +13,21 @@ class Budget {
     }
     getTotalExpenses() { // method to calculate all the expenses amounts in the this.expenses array
         return this.expenses.reduce((total, item) => total + item.amount, 0);
+    }
+    // method to add income entry
+    addIncome(description, amount) {
+        if (!description || amount <= 0) { // checks that "description" isn't missing or that amount isn't less than or equal to 0
+            throw new Error("Invalid income entry"); // shows an error message
+        }
+        // if the parameters are met it will be added to income array as key-value pairs
+        this.income.push({ description, amount });
+    }
+    // method to add expense entry
+    addExpenses(description, amount) {
+        if (!description || amount <= 0) { // checks that "description" isn't missing or that amount isn't less than or equal to 0
+            throw new Error("Invalid income entry"); // shows an error message
+        }
+        // if the parameters are met it will be added to expenses array as key-value pairs
+        this.expenses.push({ description, amount });
     }
 }
